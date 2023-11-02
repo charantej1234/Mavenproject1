e99c95bfb144a5f6e6a4ed02160583f3ef1a55e7
charan <charantejurs@gmail.com>
2023-11-02 13:07:25 +0530

    Demo for MavenProject

---
+++ src/main/java/SE_Group/charanMavenProject1/App.java
@@ -0,0 +1,13 @@
+package SE_Group.charanMavenProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! i am charan tej........");
+    }
+}
